c729c2513207d809a0ee90d88301c2aecdb807f9
Varunappalla1993 <an.varun1993@gmail.com>
2026-03-10 13:27:52 +0530

    AWS VPC

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,21 @@
-# Day 36
+# Day 36 and 37
 
 output "azs_info" {
     value = data.aws_availability_zones.available
+}
+
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+    value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
 }
