7359da192f910edc669f06c28ac3fd3a77b26198
Varunappalla1993 <an.varun1993@gmail.com>
2026-04-02 16:29:36 +0530

    locals

--- locals.tf
+++ locals.tf
@@ -1,30 +1,30 @@
 # Day 36
 
 locals {
-  common_tags={
-    Project=var.project
-    Environment=var.environment
-    Terraform= "true"
+  common_tags = {
+    Project     = var.project
+    Environment = var.environment
+    Terraform   = "true"
   }
-  
-  vpc_final_tags= merge(
+
+  vpc_final_tags = merge(
     local.common_tags,
     {
-    Name="${var.project}-${var.environment}"
+      Name = "${var.project}-${var.environment}"
     },
     var.vpc_tags
-    )
+  )
 
-    igw_final_tags=merge(
-      local.common_tags,
-      {
-           Name="${var.project}-${var.environment}"
-      },
-      
-      var.igw_tags
-    )
+  igw_final_tags = merge(
+    local.common_tags,
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+
+    var.igw_tags
+  )
 
 
-    az_names = slice(data.aws_availability_zones.available.names, 0, 2)
+  az_names = slice(data.aws_availability_zones.available.names, 0, 2)
 
 }
